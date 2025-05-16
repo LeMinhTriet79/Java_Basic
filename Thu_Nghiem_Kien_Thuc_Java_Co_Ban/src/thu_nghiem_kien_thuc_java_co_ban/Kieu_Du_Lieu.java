513f54c1eb87aa1998f104370f415a138bff9836
LeMinhTriet79 <trietleminh7979@gmail.com>
2025-05-16 10:59:14 +0700

    up

--- Thu_Nghiem_Kien_Thuc_Java_Co_Ban/src/thu_nghiem_kien_thuc_java_co_ban/Kieu_Du_Lieu.java
+++ Thu_Nghiem_Kien_Thuc_Java_Co_Ban/src/thu_nghiem_kien_thuc_java_co_ban/Kieu_Du_Lieu.java
@@ -12,14 +12,39 @@ public class Kieu_Du_Lieu {
     public static void main(String[] args) {
         //byte : It is 1 byte(8-bits) integer data type. 
         //Value range from -128 to 127. Default value zero.
-        byte a = -120;
+        byte a = -128;
         System.out.println("a: "+ a);
-        byte b = 0 ;
+        byte b = 127 ;
         System.out.println("b: "+ b);
         //Gãy cổ
 //        byte c = -129 ;
 //        System.out.println("c: "+ c);
-//        byte d = 129 ;
+//        byte d = 128 ;
 //        System.out.println("d: "+ d);
+
+/*
+Integer
+This group includes byte, short, int, long
+
+byte : It is 1 byte(8-bits) integer data type. Value range from -128 to 127. Default value zero. example: byte b=10;
+
+short : It is 2 bytes(16-bits) integer data type. Value range from -32768 to 32767. Default value zero. example: short s=11;
+
+int : It is 4 bytes(32-bits) integer data type. Value range from -2147483648 to 2147483647. Default value zero. example: int i=10;
+
+long : It is 8 bytes(64-bits) integer data type. Value range from -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807. Default value zero. example: long l=100012;  
+*/
+
+    // short type
+    	short s = 20;
+    	System.out.println("s= "+s);
+    	
+    	// int type
+    	int i = 20;
+    	System.out.println("i= "+i);
+    	
+    	// long type
+    	long l = 20;
+    	System.out.println("l= "+l);
     }
 }
