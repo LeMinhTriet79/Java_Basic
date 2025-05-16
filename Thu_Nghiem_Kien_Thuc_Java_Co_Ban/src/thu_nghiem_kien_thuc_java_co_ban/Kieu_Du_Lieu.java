b5ea7f251ca40f229dcbf0f74519fc7f00a5716e
LeMinhTriet79 <trietleminh7979@gmail.com>
2025-05-16 09:46:21 +0700

    up

---
+++ Thu_Nghiem_Kien_Thuc_Java_Co_Ban/src/thu_nghiem_kien_thuc_java_co_ban/Kieu_Du_Lieu.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package thu_nghiem_kien_thuc_java_co_ban;
+
+/**
+ *
+ * @author Minh Triet
+ */
+public class Kieu_Du_Lieu {
+    public static void main(String[] args) {
+        byte a = 20;
+        System.out.println("a: "+ a);
+    }
+}
