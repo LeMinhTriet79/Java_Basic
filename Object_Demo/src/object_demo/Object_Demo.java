0799628ae86b679010b828484a81f550d0c7cad1
LeMinhTriet79 <trietleminh7979@gmail.com>
2025-08-09 15:05:12 +0700

    up

--- Object_Demo/src/object_demo/Object_Demo.java
+++ Object_Demo/src/object_demo/Object_Demo.java
@@ -6,8 +6,16 @@ package object_demo;
 public class Object_Demo {
     public static void main(String[] args) {
         Student a = new Student(1, "Nguyễn Văn Hoài" , 16);
-        
         System.out.println(a);
         
+        //===================================//
+        
+        Student_02 b = new Student_02(1, "Phạm Võ Khải Anh", 21);
+        
+        Student_02 c = new Student_02();
+        
+        
+        System.out.println(b);
+        System.out.println(c);
     }    
 }
--- Object_Demo/src/object_demo/Student_02.java
+++ Object_Demo/src/object_demo/Student_02.java
@@ -4,5 +4,47 @@
  * @author Le Minh Triet
  */
 public class Student_02 {
+   int id;
+   String name;
+   int yob;
+
+  public Student_02(){}
+  
+    public Student_02(int id, String name, int yob) {
+        this.id = id;
+        this.name = name;
+        this.yob = yob;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getYob() {
+        return yob;
+    }
+
+    public void setYob(int yob) {
+        this.yob = yob;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" + "id=" + id + ", name=" + name + ", yob=" + yob + '}';
+    }
+  
     
+ 
 }
