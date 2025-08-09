9e6930d12c12bbc994ef2fd73b1ffa49d0f0a53e
LeMinhTriet79 <trietleminh7979@gmail.com>
2025-08-09 14:59:22 +0700

    up

--- Object_Demo/src/object_demo/Object_Demo.java
+++ Object_Demo/src/object_demo/Object_Demo.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package object_demo;
 
 /**
@@ -10,7 +5,6 @@ package object_demo;
  * @author Le Minh Triet
  */
 public class Object_Demo {
-
     public static void main(String[] args) {
         // TODO code application logic here
     }
