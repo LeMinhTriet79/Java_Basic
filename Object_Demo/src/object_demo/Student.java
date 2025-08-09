a58fece26d9feb7ca4855a3ab59ef9831ab44967
LeMinhTriet79 <trietleminh7979@gmail.com>
2025-08-09 14:58:46 +0700

    up

--- Object_Demo/src/object_demo/Student.java
+++ Object_Demo/src/object_demo/Student.java
@@ -10,5 +10,47 @@ package object_demo;
  * @author Le Minh Triet
  */
 public class Student {
+  private int id;
+  private String name;
+  private int yob;
+
+  public Student(){}
+  
+    public Student(int id, String name, int yob) {
+        this.id = id;
+        this.name = name;
+        this.yob = yob;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getYob() {
+        return yob;
+    }
+
+    public void setYob(int yob) {
+        this.yob = yob;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" + "id=" + id + ", name=" + name + ", yob=" + yob + '}';
+    }
+  
     
+  
 }
