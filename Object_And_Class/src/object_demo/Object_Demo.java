f611fb21729589799721a6de4311c5e7213a5101
LeMinhTriet79 <trietleminh7979@gmail.com>
2025-08-09 15:49:21 +0700

    up

--- Object_And_Class/src/object_demo/Object_Demo.java
+++ Object_And_Class/src/object_demo/Object_Demo.java
@@ -31,8 +31,11 @@ public class Object_Demo {
         //Class chứa phuơng thức
         Student_With_Methods t0 = new Student_With_Methods(3, "Võ Thị Diễm Em", 17);
         System.out.println(t0);
-        System.out.println("Đối tuợng này đã đủ 18 tuổi chưa?"
+        //hàm trả về boolean
+        System.out.println("Đối tuợng" + t0.getName() +" đã đủ 18 tuổi chưa?"
                 + "Câu trả lời là: " + t0.check_Yob(t0.getYob()));
-        
+       //hàm trả về chuỗi
+        System.out.println("Đối tuợng này đã đủ 18 tuổi chưa?"
+                + "Câu trả lời là: " + t0.kiem_tra_tuoi(t0.getYob()));
     }    
 }
