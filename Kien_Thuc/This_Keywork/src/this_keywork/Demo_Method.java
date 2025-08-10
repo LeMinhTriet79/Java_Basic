e49a68eb9b0d8712418933a24c1c9e4f9e946300
LeMinhTriet79 <trietleminh7979@gmail.com>
2025-08-10 15:09:29 +0700

    up

---
+++ Kien_Thuc/This_Keywork/src/this_keywork/Demo_Method.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package this_keywork;
+
+/**
+ *
+ * @author Le Minh Triet
+ */
+public class Demo_Method {
+    
+    public void method_01(){
+        System.out.println("Me");
+    }
+}
