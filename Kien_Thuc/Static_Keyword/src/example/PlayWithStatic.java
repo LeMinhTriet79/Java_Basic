f59e986b44b44faa28d2c5f2084a634c44e531af
LeMinhTriet79 <trietleminh7979@gmail.com>
2025-08-20 15:59:05 +0700

    up

---
+++ Kien_Thuc/Static_Keyword/src/example/PlayWithStatic.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package example;
+
+/**
+ *
+ * @author Le Minh Triet
+ */
+public class PlayWithStatic {
+    
+    String name;
+
+    public PlayWithStatic(String name) {
+        this.name = name;
+       
+    }
+
+    public PlayWithStatic() {
+       
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    
+    
+    //KHÔNG CÓ STATIC
+    public void daoLy(String name){
+        System.out.println( name + "Cố gắng học hành, một ngày chỉ ngủ 8 tiếng thôi"
+                + "\n đừng có ngủ trưa mà tốn thời gian code");
+    }
+    
+    //CÓ STATIC
+    public static void chuiThe(){
+        System.out.println("MẸ KIẾP THỜI GIAN MÀY NẰM NGỦ NÓ"
+                + "\n CÒN NHIỀU HƠN THỜI GIAN MÀY ÔN HỌC"
+                + "\n ÔNG BÀ GIÀ XƯA CÓ CÂU 'HAM ĂN THÌ NGU HAM NGỦ THÌ LÚ' "
+                + "\n HỌC HÀNH MÀ ĐÉO CHỊU KHÓ THÌ VĨNH VIỄN ĐÉO CÓ NGÀY THÀNH CÔNG");
+    }
+}
