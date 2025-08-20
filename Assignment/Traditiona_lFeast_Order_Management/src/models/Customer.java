e9567667c809eff77a688adc07300f3d70b8b615
LeMinhTriet79 <trietleminh7979@gmail.com>
2025-08-20 09:46:55 +0700

    up

--- Assignment/Traditiona_lFeast_Order_Management/src/models/Customer.java
+++ Assignment/Traditiona_lFeast_Order_Management/src/models/Customer.java
@@ -6,11 +6,39 @@
 package models;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 /**
  *
  * @author Le Minh Triet
  */
 public class Customer implements Serializable{
+    String name;
+    String yob;
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Customer other = (Customer) obj;
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (!Objects.equals(this.yob, other.yob)) {
+            return false;
+        }
+        return true;
+    }
+    
     
 }
