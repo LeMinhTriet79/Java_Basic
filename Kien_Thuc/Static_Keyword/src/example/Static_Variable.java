5afa16521ddf49a07edb3fd4cc8986405cafde05
LeMinhTriet79 <trietleminh7979@gmail.com>
2025-08-20 16:18:41 +0700

    up

--- Kien_Thuc/Static_Keyword/src/example/Static_Variable.java
+++ Kien_Thuc/Static_Keyword/src/example/Static_Variable.java
@@ -10,6 +10,13 @@ package example;
  * @author Le Minh Triet
  */
 public class Static_Variable {
+    /*
+static variable (biến tĩnh)
+Tính chất
+Dùng chung cho mọi object của lớp.
+Khởi tạo khi lớp được initialize lần đầu; sống đến khi lớp bị dỡ (thường là hết chương trình).
+Mặc định về phạm vi truy cập (public/private/...) y như biến thường.
+    */
    public static int count = 0;
 
     public Static_Variable() {
