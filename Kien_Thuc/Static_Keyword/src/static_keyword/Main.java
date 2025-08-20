1dc4b27c3e0d2c45ceb36bb77dd1a3f45ada5501
LeMinhTriet79 <trietleminh7979@gmail.com>
2025-08-20 16:03:43 +0700

    up

--- Kien_Thuc/Static_Keyword/src/static_keyword/Main.java
+++ Kien_Thuc/Static_Keyword/src/static_keyword/Main.java
@@ -1,21 +1,35 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package static_keyword;
+
+import example.PlayWithStatic;
 
 /**
  *
  * @author Le Minh Triet
  */
 public class Main {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+public static void main(String[] args) {
+         // CHƠI LẦN ĐẦU ĐÉO CÓ STATIC
+        System.out.println("CHƠI LẦN THỨ NHỨT/NHẤT KHÔNG DÙNG STATIC: ");
+        String ten = "Văn Hoài: ";
+        PlayWithStatic choi_lan_thu_nhat = new PlayWithStatic(ten);
+        choi_lan_thu_nhat.daoLy(ten);
+        // TAO THÍCH MUỐN SÀI THÌ NEW NEW NEW THÍCH THÌ NEW , KHÔNG THÍCH CŨNG NEW
+        //NEW TỚI NỔI TRÀN BỘ NHỚ, HƯ MÁY ĐẬP MUA MÁY MỚI
+        //KHÔNG NEW THÌ CHÚNG KHÔNG CHO SÀI CHO CHƠI
+        String ten1 = "Khải Anh: ";
+        PlayWithStatic choi_1 = new PlayWithStatic(ten1);
+        choi_1.daoLy(ten1);
+        // cứ thế mà tạo thêm nhiều đối tuợng chơi 2, chơi 3, chơi 4, chơi 5...
+        //CHO HAO TỔN RAM CHƠI
+        //=============================================//
+       
+        //CHƠI CÓ STATIC 
+        //TAO ĐÉO CẦN NEW ĐỐI TUỢNG GÌ HẾT, HỔNG TẠO GÌ HẾT
+        //QUẤT THẲNG
+        System.out.println(); //xuống dòng nó như cái dấu ENTER, cho dễ nhìn
+        System.out.println("CHƠI LẦN THỨ NHÌ: CÓ DÙNG STATIC");
+        PlayWithStatic.chuiThe();
+        
     }
     
 }
