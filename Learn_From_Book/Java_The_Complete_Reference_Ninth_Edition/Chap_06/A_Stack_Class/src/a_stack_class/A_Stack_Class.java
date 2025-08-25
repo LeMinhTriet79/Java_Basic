61534eda56390d36d65a64724f3d44c9fa319e53
LeMinhTriet79 <trietleminh7979@gmail.com>
2025-08-25 10:09:33 +0700

    up

--- Learn_From_Book/Java_The_Complete_Reference_Ninth_Edition/Chap_06/A_Stack_Class/src/a_stack_class/A_Stack_Class.java
+++ Learn_From_Book/Java_The_Complete_Reference_Ninth_Edition/Chap_06/A_Stack_Class/src/a_stack_class/A_Stack_Class.java
@@ -15,16 +15,32 @@ public class A_Stack_Class {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        // MỖI STACK CÓ VÙNG NHỚ RIÊNG
+        /*
+        push tăng tos rồi gán phần tử.
+        pop lấy phần tử tại tos rồi giảm tos.
+        */
         Stack s1 = new Stack();
         Stack s2 = new Stack();
         
         for(int i = 0; i < 10; i++){
-            
+            s1.push(i);
         }
         
-        for(int i = 0; i < 10; i++){
-            
+        for(int i = 10; i < 20; i++){
+            s2.push(i);
         }
+        
+        System.out.println("Stack s1: ");
+        for(int i = 0; i < 10; i ++){
+            System.out.println(s1.pop());
+        }
+        
+        System.out.println("Stack s2: ");
+        for(int i = 10; i < 20; i ++){
+            System.out.println(s2.pop());
+        }
+        //System.out.println(s2.stck[1]);
         
     }
     
