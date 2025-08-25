390ec215abf1df18cebff51af265fa6cd13e1c8b
LeMinhTriet79 <trietleminh7979@gmail.com>
2025-08-25 11:38:43 +0700

    up

---
+++ Learn_From_Book/Java_The_Complete_Reference_Ninth_Edition/Chap_07/7.1_Overloading_Methods/src/pkg7/pkg1_overloading_methods/Overload_Demo.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg7.pkg1_overloading_methods;
+
+/**
+ *
+ * @author Le Minh Triet
+ */
+public class Overload_Demo {
+    //NẠP CHỒNG PHUƠNG THỨC
+    void test(){
+        System.out.println("No parameters");
+    }
+    
+    void test(int a){
+        System.out.println("a: " + a);
+    }
+    
+}
