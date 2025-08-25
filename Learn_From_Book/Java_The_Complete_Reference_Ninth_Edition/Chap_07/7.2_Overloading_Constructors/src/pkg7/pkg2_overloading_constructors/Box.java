18a3bc69329eb25da70c5bc3587c8e7928443bde
LeMinhTriet79 <trietleminh7979@gmail.com>
2025-08-25 12:11:09 +0700

    up

--- Learn_From_Book/Java_The_Complete_Reference_Ninth_Edition/Chap_07/7.2_Overloading_Constructors/src/pkg7/pkg2_overloading_constructors/Box.java
+++ Learn_From_Book/Java_The_Complete_Reference_Ninth_Edition/Chap_07/7.2_Overloading_Constructors/src/pkg7/pkg2_overloading_constructors/Box.java
@@ -10,5 +10,26 @@ package pkg7.pkg2_overloading_constructors;
  * @author Le Minh Triet
  */
 public class Box {
+    double width, height, depth;
+
+    //Constructor không tham số
+    public Box() {
+    }
     
+    //Constructor có một tham số (hình lập phuơng)
+    Box(double len){
+        width = height = depth = len;
+    }
+    
+    //constructor có 3 tham số
+
+    public Box(double width, double height, double depth) {
+        this.width = width;
+        this.height = height;
+        this.depth = depth;
+    }
+    
+   double volume(){
+      return width * height * depth;
+   }
 }
