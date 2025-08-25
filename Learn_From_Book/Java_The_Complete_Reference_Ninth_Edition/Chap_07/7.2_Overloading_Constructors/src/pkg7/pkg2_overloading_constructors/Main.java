90e6546098b46599f7a59288cf34d40677d0a734
LeMinhTriet79 <trietleminh7979@gmail.com>
2025-08-25 12:12:50 +0700

    up

--- Learn_From_Book/Java_The_Complete_Reference_Ninth_Edition/Chap_07/7.2_Overloading_Constructors/src/pkg7/pkg2_overloading_constructors/Main.java
+++ Learn_From_Book/Java_The_Complete_Reference_Ninth_Edition/Chap_07/7.2_Overloading_Constructors/src/pkg7/pkg2_overloading_constructors/Main.java
@@ -13,9 +13,11 @@ public class Main {
  public static void main(String[] args) {
         Box b1 = new Box();
         Box b2 = new Box(10);
-        Box b3 = new Box(width, height, depth);
+        Box b3 = new Box(10, 20, 30);
         
-        
+        System.out.println("Volume b1 = " + b1.volume());
+        System.out.println("Volume b2 = " + b2.volume());
+        System.out.println("Volume b3 = " + b3.volume());
     }
     
 }
