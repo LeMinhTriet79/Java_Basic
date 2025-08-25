4405cd488e1189406ecd5f220bdf04cfc8f7f464
LeMinhTriet79 <trietleminh7979@gmail.com>
2025-08-25 09:55:41 +0700

    up

--- Learn_From_Book/Java_The_Complete_Reference_Ninth_Edition/Chap_06/A_Stack_Class/src/a_stack_class/A_Stack_Class.java
+++ Learn_From_Book/Java_The_Complete_Reference_Ninth_Edition/Chap_06/A_Stack_Class/src/a_stack_class/A_Stack_Class.java
@@ -15,7 +15,9 @@ public class A_Stack_Class {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int a = 1;
+        
+        System.out.println(++a);
     }
     
 }
