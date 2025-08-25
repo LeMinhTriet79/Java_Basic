99d1ea5dc251046a712acf72686a63af6feb1127
LeMinhTriet79 <trietleminh7979@gmail.com>
2025-08-25 12:08:10 +0700

    up

---
+++ Learn_From_Book/Java_The_Complete_Reference_Ninth_Edition/Chap_07/7.2_Overloading_Constructors/src/pkg7/pkg2_overloading_constructors/Box.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg7.pkg2_overloading_constructors;
+
+/**
+ *
+ * @author Le Minh Triet
+ */
+public class Box {
+    
+}
