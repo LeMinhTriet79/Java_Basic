bd9420bc804e6520a2379d702be31970084c0c72
LeMinhTriet79 <trietleminh7979@gmail.com>
2025-10-22 09:17:48 +0700

    test wifi

--- demo_02/src/main/java/minecraft/se3979/Main.java
+++ demo_02/src/main/java/minecraft/se3979/Main.java
@@ -5,5 +5,7 @@ package minecraft.se3979;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Xin kính chào bà con!");
+        System.out.println("Không có Thượng-đế nào ngoài Allah\n" +
+                "Muhammad là sứ giả cuối cùng của Thượng-đế");
     }
 }
